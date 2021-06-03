0caeb89b7fd7bebf5fc70dc62de23fea8aaadf11
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-06-03 20:45:46 -0300

    Update

---
+++ src/_LABORATORIO/MetodosStrings.java
@@ -0,0 +1,18 @@
+package _LABORATORIO;
+
+public class MetodosStrings {
+
+	public static void main(String[] args) {
+
+		String str = "Estoy quemado";
+
+		// Devuelve el caracter del indice especificado
+		System.out.println(str.charAt(0));
+		// Devuelve el indice del caracter especificado
+		System.out.println(str.indexOf('E'));
+		// Devuelve el indice de la subcadena
+		System.out.println(str.indexOf("que"));
+
+	}
+
+}
--- src/_LABORATORIO/Prueba.java
+++ src/_LABORATORIO/Prueba.java
@@ -4,10 +4,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 
-		String str = "abc";
-
-		System.out.println(str.indexOf(97));
-
+		
 	}
 
 }
