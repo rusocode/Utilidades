b5095c66d056063fd72c6995ddfae4512f3bc894
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-06-22 20:47:11 -0300

    Update

--- src/io/FlujoDeBytes.java
+++ src/io/FlujoDeBytes.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Para los archivos binarios se usan las clases abstractas InputStream y OutputStram que leen y escriben flujos de
@@ -61,28 +63,34 @@ public class FlujoDeBytes {
 
 		char[] caracteres = texto.toCharArray();
 
-		try {
+		// https://stackoverflow.com/questions/25546869/how-i-can-write-a-file-using-relative-path-in-java
+		Path relativePath = Paths.get(TEXT_FILE_PATH + file);
+		Path absolutePath = relativePath.toAbsolutePath();
 
-			URL url = getClass().getClassLoader().getResource(TEXT_FILE_PATH + file);
+		System.out.println(absolutePath.getParent());
 
-			output = new FileOutputStream(url.getFile());
-
-			System.out.println("Nombre del archivo: " + file);
-
-			for (int i = 0; i < caracteres.length; i++)
-				output.write(caracteres[i]);
-
-		} catch (FileNotFoundException e) {
-			System.err.println("El archivo no existe!");
-		} catch (IOException e) {
-			System.err.println("Error de I/O!");
-		} finally {
-			try {
-				if (output != null) output.close();
-			} catch (IOException e) {
-				System.err.println("No se pudo cerrar el flujo de salida!");
-			}
-		}
+//		try {
+//
+//			URL url = getClass().getClassLoader().getResource(TEXT_FILE_PATH + file);
+//
+//			output = new FileOutputStream(url.getFile());
+//
+//			System.out.println("Nombre del archivo: " + file);
+//
+//			for (int i = 0; i < caracteres.length; i++)
+//				output.write(caracteres[i]);
+//
+//		} catch (FileNotFoundException e) {
+//			System.err.println("El archivo no existe!");
+//		} catch (IOException e) {
+//			System.err.println("Error de I/O!");
+//		} finally {
+//			try {
+//				if (output != null) output.close();
+//			} catch (IOException e) {
+//				System.err.println("No se pudo cerrar el flujo de salida!");
+//			}
+//		}
 	}
 
 	public static void main(String[] args) {
