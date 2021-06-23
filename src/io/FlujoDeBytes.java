4a8361ccb605f2fda71a5a72591869a01ca3008a
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-06-23 12:42:24 -0300

    Update

--- src/io/FlujoDeBytes.java
+++ src/io/FlujoDeBytes.java
@@ -24,8 +24,9 @@ import java.nio.file.Paths;
 public class FlujoDeBytes {
 
 	private InputStream input;
-	private OutputStream output;
+	private FileOutputStream output;
 
+	private static final String S = File.separator;
 	private static final String TEXT_FILE_PATH = "texts" + File.separator;
 	private static final String TEXT_FILE_NAME = "Texto.txt";
 
@@ -67,36 +68,61 @@ public class FlujoDeBytes {
 		Path relativePath = Paths.get(TEXT_FILE_PATH + file);
 		Path absolutePath = relativePath.toAbsolutePath();
 
-		System.out.println(absolutePath.getParent());
+		// System.out.println(absolutePath.getParent().toString());
 
-//		try {
-//
-//			URL url = getClass().getClassLoader().getResource(TEXT_FILE_PATH + file);
-//
-//			output = new FileOutputStream(url.getFile());
-//
-//			System.out.println("Nombre del archivo: " + file);
-//
-//			for (int i = 0; i < caracteres.length; i++)
-//				output.write(caracteres[i]);
-//
-//		} catch (FileNotFoundException e) {
-//			System.err.println("El archivo no existe!");
-//		} catch (IOException e) {
-//			System.err.println("Error de I/O!");
-//		} finally {
-//			try {
-//				if (output != null) output.close();
-//			} catch (IOException e) {
-//				System.err.println("No se pudo cerrar el flujo de salida!");
-//			}
-//		}
+		// URL url = getClass().getClassLoader().getResource("texts/Texto.txt"); FUNCIONA POR LA BARRA DE MIERDA!!
+		URL url = getClass().getResource("/texts/Texto.txt");
+
+		// System.out.println(url.getPath());
+
+		// File f = new File("texts/Texto.txt");
+		// System.out.println(f.exists());
+
+		try {
+
+			// "C:"+S+"Users"+S+"juand"+S+"Documents"+S+"Eclipse - Proyectos"+S+"utilidades"+S+"assets"+S+"texts"+S+"Texto.txt"
+			// "C:\\Users\\juand\\Documents\\Eclipse - Proyectos\\utilidades\\assets\\texts\\Texto.txt"
+			// "C:/Users/juand/Documents/Eclipse - Proyectos/utilidades/assets/texts/Texto.txt"
+
+			// https://stackoverflow.com/questions/17287478/get-file-from-project-folder-java/36445369
+			ClassLoader cl = getClass().getClassLoader(); // Parece que cargando el recurso desde el loader no encuentra el archivo...
+			File f1 = new File(cl.getResource(".").getFile());
+
+			File f2 = new File(System.getProperty("user.home") + S + "Documents" + S + "Eclipse - Proyectos" + S + "utilidades" + S + "bin" + S
+					+ "texts" + S + "texto.txt");
+
+			if (f1 != null) {
+				System.out.println("Ruta absoluta = " + f1.getAbsolutePath());
+				System.out.println("Nombre del archivo = " + f1.getName());
+				System.out.println(f1.list());
+
+				// https://stackoverflow.com/questions/11553042/the-system-cannot-find-the-file-specified-in-java
+//				for (String files : f2.list())
+//					System.out.println(files);
+			}
+
+			output = new FileOutputStream(f1);
+
+			for (int i = 0; i < caracteres.length; i++)
+				output.write(caracteres[i]);
+
+		} catch (FileNotFoundException e) {
+			System.err.println("El archivo no existe!\n" + e.getMessage());
+		} catch (IOException e) {
+			System.err.println("Error de I/O!");
+		} finally {
+			try {
+				if (output != null) output.close();
+			} catch (IOException e) {
+				System.err.println("No se pudo cerrar el flujo de salida!");
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		FlujoDeBytes flujo = new FlujoDeBytes();
 		// flujo.read(TEXT_FILE_NAME);
-		flujo.write(TEXT_FILE_NAME, "Rulo quemado");
+		flujo.write(TEXT_FILE_NAME, "Rulo quemadouuaaa");
 	}
 
 }
