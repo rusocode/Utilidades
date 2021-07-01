c013eaf8b938d0e7162a650b669a6167ee4111ce
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-07-01 12:32:58 -0300

    Update

---
+++ src/io/InputStreamProcessingTemplate.java
@@ -0,0 +1,54 @@
+package io;
+
+import java.io.*;
+
+// http://tutorials.jenkov.com/java-exception-handling/exception-handling-templates.html
+public class InputStreamProcessingTemplate {
+
+	public static void process(String fileName, InputStreamProcessor processor) {
+
+		IOException processException = null;
+		InputStream input = null;
+
+		try {
+
+			input = new FileInputStream(fileName);
+			processor.process(input);
+
+		} catch (IOException e) {
+
+			processException = e;
+
+		} finally {
+
+			if (input != null) {
+
+				try {
+
+					input.close();
+
+				} catch (IOException e) {
+
+					if (processException != null) {
+//		                    throw new MyException(processException, e,
+//		                      "Error message..." +
+//		                      fileName);
+					} else {
+//		                    throw new MyException(e,
+//		                        "Error closing InputStream for file " +
+//		                        fileName;
+					}
+				}
+
+			}
+
+			if (processException != null) {
+//		              throw new MyException(processException,
+//		                "Error processing InputStream for file " +
+//		                    fileName;
+			}
+		}
+
+	}
+
+}
---
+++ src/io/InputStreamProcessor.java
@@ -0,0 +1,9 @@
+package io;
+
+import java.io.*;
+
+public interface InputStreamProcessor {
+
+	public void process(InputStream input) throws IOException;
+
+}
